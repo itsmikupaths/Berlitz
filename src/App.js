13cc268b069e5e5370977e7a54d03d27bf797b5d
John Mike Patay <johnmike@digitalspaceexplorer.com>
2022-03-04 23:01:48 +0800

    installed font-awesome for icons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.scss';
+
+// font-awesome icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="back-button">
+          <FontAwesomeIcon icon={faArrowLeftLong} />
+          All products
+        </div>
       </header>
     </div>
   );
